310840981b828664f0417ba488caa7bcfdadcc5e
cheyuwu <frank890417@gmail.com>
2018-03-29 11:33:40 +0800

    update link

--- resources/views/emails/testemail.blade.php
+++ resources/views/emails/testemail.blade.php
@@ -3,7 +3,7 @@
 		
 	</head>
 	<body>
-		<img src="http://api.taipeisoundscape.com/img/mail_header_2018.jpg" style="width: 100%;" alt="">
+		<img src="http://api.taipeisoundscape.com/img/main_header_2018.jpg" style="width: 100%;" alt="">
 		<h3>{{$user}}您好：</h3><br>
 		<p>
 			<p>感謝您參與「站體環境音樂-隱音」徵選活動<br>已成功收到您的報名資訊<br>徵選結果將於6/5 12:00公佈<br>敬請靜候佳音<br><br>
