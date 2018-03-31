a549c8064f483897dbb4909ef538c88f91338b52
cheyuwu <frank890417@gmail.com>
2018-03-31 09:57:50 +0800

    update email

--- resources/views/emails/testemail.blade.php
+++ resources/views/emails/testemail.blade.php
@@ -6,7 +6,7 @@
 		<img src="http://api.taipeisoundscape.com/img/main_header_2018.jpg" style="width: 100%;" alt="">
 		<h3>{{$user}}您好：</h3><br>
 		<p>
-			<p>感謝您參與「站體環境音樂-隱音」徵選活動<br>已成功收到您的報名資訊<br>徵選結果將於6/5 12:00公佈<br>敬請靜候佳音<br><br>
+			<p>感謝您參與「站體環境音樂-隱音」徵選活動<br>已成功收到您的報名資訊<br>徵選結果將於6/5 12:00公佈於官⽅網站及Facebook活動粉絲專頁<br>敬請靜候佳音<br><br>
 			<br>
 			Thank you for application. You’ve already upload your work to Taipei Soundscape<Br>
 Program “MRT Station Ambient Music Competition - Sound of Silence”. Award announcement will be post on the website of the competition on June 5, 2018 at 12 P.M.
