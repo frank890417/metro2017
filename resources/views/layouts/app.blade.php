e0659335fc30440227734fddb3f374d0fe410c83
cheyuwu <frank890417@gmail.com>
2018-03-31 22:22:45 +0800

    uodate transform box

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css">
+    <style>
+        svg *{
+            transform-box: fill-box
+        }
+    </style>
 </head>
 <body>
     <div id="app">
